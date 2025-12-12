969b622f6d246735716b5a29ee943dc238894f8a
rustjesus <yeric12345@gmail.com>
2025-12-11 18:20:10 -0600

    search and find

--- LibraryOfBabel/Form1.Designer.cs
+++ LibraryOfBabel/Form1.Designer.cs
@@ -34,29 +34,43 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.tbWall = new System.Windows.Forms.TrackBar();
+            this.tbShelf = new System.Windows.Forms.TrackBar();
+            this.tbVolume = new System.Windows.Forms.TrackBar();
+            this.rtbHex = new System.Windows.Forms.RichTextBox();
+            this.findVolButton2 = new System.Windows.Forms.Button();
+            this.lblShelf = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblWall = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pageRtb = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbShelf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
             this.SuspendLayout();
             // 
             // rtbOutput
             // 
-            this.rtbOutput.Location = new System.Drawing.Point(60, 63);
+            this.rtbOutput.Location = new System.Drawing.Point(12, 274);
             this.rtbOutput.Name = "rtbOutput";
-            this.rtbOutput.Size = new System.Drawing.Size(750, 453);
+            this.rtbOutput.Size = new System.Drawing.Size(768, 437);
             this.rtbOutput.TabIndex = 1;
             this.rtbOutput.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Location = new System.Drawing.Point(690, 216);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(93, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "search";
+            this.button1.Text = "Search Phrase";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(38, 12);
+            this.txtSearch.Location = new System.Drawing.Point(15, 194);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(669, 45);
             this.txtSearch.TabIndex = 3;
@@ -65,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-2, 17);
+            this.label1.Location = new System.Drawing.Point(12, 178);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 4;
@@ -74,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Location = new System.Drawing.Point(12, 258);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 5;
@@ -83,17 +97,134 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(732, 44);
+            this.label3.Location = new System.Drawing.Point(3, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "v0.0.2";
             // 
+            // tbWall
+            // 
+            this.tbWall.Location = new System.Drawing.Point(46, 102);
+            this.tbWall.Maximum = 4;
+            this.tbWall.Minimum = 1;
+            this.tbWall.Name = "tbWall";
+            this.tbWall.Size = new System.Drawing.Size(104, 45);
+            this.tbWall.TabIndex = 9;
+            this.tbWall.Value = 1;
+            this.tbWall.Scroll += new System.EventHandler(this.wall_trackBar1_Scroll);
+            // 
+            // tbShelf
+            // 
+            this.tbShelf.Location = new System.Drawing.Point(256, 102);
+            this.tbShelf.Maximum = 5;
+            this.tbShelf.Minimum = 1;
+            this.tbShelf.Name = "tbShelf";
+            this.tbShelf.Size = new System.Drawing.Size(104, 45);
+            this.tbShelf.TabIndex = 10;
+            this.tbShelf.Value = 1;
+            this.tbShelf.Scroll += new System.EventHandler(this.shelf_trackBar2_Scroll);
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.Location = new System.Drawing.Point(449, 102);
+            this.tbVolume.Maximum = 32;
+            this.tbVolume.Minimum = 1;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(104, 45);
+            this.tbVolume.TabIndex = 11;
+            this.tbVolume.Value = 1;
+            this.tbVolume.Scroll += new System.EventHandler(this.vol_trackBar3_Scroll);
+            // 
+            // rtbHex
+            // 
+            this.rtbHex.Location = new System.Drawing.Point(46, 31);
+            this.rtbHex.Name = "rtbHex";
+            this.rtbHex.Size = new System.Drawing.Size(348, 24);
+            this.rtbHex.TabIndex = 12;
+            this.rtbHex.Text = "";
+            this.rtbHex.TextChanged += new System.EventHandler(this.hex_richTextBox1_TextChanged);
+            // 
+            // findVolButton2
+            // 
+            this.findVolButton2.Location = new System.Drawing.Point(690, 134);
+            this.findVolButton2.Name = "findVolButton2";
+            this.findVolButton2.Size = new System.Drawing.Size(75, 23);
+            this.findVolButton2.TabIndex = 13;
+            this.findVolButton2.Text = "Find";
+            this.findVolButton2.UseVisualStyleBackColor = true;
+            this.findVolButton2.Click += new System.EventHandler(this.findVolButton2_Click);
+            // 
+            // lblShelf
+            // 
+            this.lblShelf.AutoSize = true;
+            this.lblShelf.Location = new System.Drawing.Point(201, 102);
+            this.lblShelf.Name = "lblShelf";
+            this.lblShelf.Size = new System.Drawing.Size(32, 13);
+            this.lblShelf.TabIndex = 14;
+            this.lblShelf.Text = "shelf:";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(385, 102);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(44, 13);
+            this.lblVolume.TabIndex = 15;
+            this.lblVolume.Text = "volume:";
+            // 
+            // lblWall
+            // 
+            this.lblWall.AutoSize = true;
+            this.lblWall.Location = new System.Drawing.Point(12, 102);
+            this.lblWall.Name = "lblWall";
+            this.lblWall.Size = new System.Drawing.Size(28, 13);
+            this.lblWall.TabIndex = 16;
+            this.lblWall.Text = "wall:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Hex:";
+            // 
+            // pageRtb
+            // 
+            this.pageRtb.Location = new System.Drawing.Point(680, 99);
+            this.pageRtb.Name = "pageRtb";
+            this.pageRtb.Size = new System.Drawing.Size(100, 25);
+            this.pageRtb.TabIndex = 18;
+            this.pageRtb.Text = "";
+            this.pageRtb.TextChanged += new System.EventHandler(this.pageRtb_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(639, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Page:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 528);
+            this.ClientSize = new System.Drawing.Size(797, 723);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pageRtb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblWall);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblShelf);
+            this.Controls.Add(this.findVolButton2);
+            this.Controls.Add(this.rtbHex);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.tbShelf);
+            this.Controls.Add(this.tbWall);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -102,6 +233,9 @@
             this.Controls.Add(this.rtbOutput);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbShelf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,6 +248,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar tbWall;
+        private System.Windows.Forms.TrackBar tbShelf;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.RichTextBox rtbHex;
+        private System.Windows.Forms.Button findVolButton2;
+        private System.Windows.Forms.Label lblShelf;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblWall;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox pageRtb;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- LibraryOfBabel/Form1.cs
+++ LibraryOfBabel/Form1.cs
@@ -14,12 +14,38 @@ namespace LibraryOfBabel
         {
             InitializeComponent();
             rtbOutput.ReadOnly = true;
-            rtbOutput.Cursor = Cursors.Default;
-            rtbOutput.ShortcutsEnabled = false;
-            rtbOutput.TabStop = false;
+            //rtbOutput.Cursor = Cursors.Default;
+            //rtbOutput.ShortcutsEnabled = false;
+            //rtbOutput.TabStop = false;
             rtbOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblVolume.Text = "Volume: 1";
+            lblShelf.Text = "Shelf: 1";
+            lblWall.Text = "Wall: 1";
+            pageRtb.Text = "1";
+            string hex = "00000000000000000000";
+            rtbHex.Text = hex;
+            GoToPage(hex, 1, 1, 1, 1);
 
+        }
+        private void GoToPage(string hex, int wall, int shelf, int volume, int page)
+        {
+            // Generate page text
+            string pageText = GeneratePage(hex, wall, shelf, volume, page);
 
+            // Display nicely
+            rtbOutput.Text =
+                $"Location:\n" +
+                $"Hex: {hex}\n" +
+                $"Wall: {wall}\n" +
+                $"Shelf: {shelf}\n" +
+                $"Volume: {volume}\n" +
+                $"Page: {page}\n\n" +
+                $"=== PAGE TEXT ===\n" +
+                $"{pageText}";
+
+            // Reset highlighting
+            rtbOutput.Select(0, 0);
+            rtbOutput.SelectionColor = System.Drawing.Color.Black;
         }
 
         // ------------------------------
@@ -87,7 +113,7 @@ namespace LibraryOfBabel
             return sb.ToString();
         }
         private (string hex, int wall, int shelf, int volume, int page, int insertIndex)
-    LocationFromPhrase(string phrase)
+        LocationFromPhrase(string phrase)
         {
             using (SHA256 sha = SHA256.Create())
             {
@@ -100,44 +126,56 @@ namespace LibraryOfBabel
                 int volume = (hash[22] % 32) + 1;
                 int page = (hash[23] % 410) + 1;
 
-                int insertIndex = hash[24] % (PageLength - phrase.Length);
+                int insertIndex = hash[24] % (PageLength - phrase.Length); // keep for internal use
 
                 return (hex, wall, shelf, volume, page, insertIndex);
             }
         }
 
+
+        // Class-level variable to store last searched phrase and location
+        private string lastSearchedPhrase = null;
+        private (string hex, int wall, int shelf, int volume, int page, int insertIndex)? lastPhraseLocation = null;
+
+        // Update button1_Click
         private void button1_Click(object sender, EventArgs e)
         {
-            string phrase = txtSearch.Text.Trim(); // <-- keep capitalization
-
+            string phrase = txtSearch.Text.Trim();
             if (phrase.Length == 0)
             {
                 MessageBox.Show("Enter a phrase to search.");
                 return;
             }
 
-            // Deterministic location lookup
             var loc = LocationFromPhrase(phrase);
 
-            // Generate the page guaranteed to contain the phrase
             string pageText = GeneratePageWithPhrase(
                 loc.hex, loc.wall, loc.shelf, loc.volume, loc.page,
                 phrase, loc.insertIndex);
 
-            // Display results
             rtbOutput.Text =
                 $"Your phrase exists at:\n" +
                 $"Hex: {loc.hex}\n" +
                 $"Wall: {loc.wall}\n" +
                 $"Shelf: {loc.shelf}\n" +
                 $"Volume: {loc.volume}\n" +
-                $"Page: {loc.page}\n" +
-                $"Index: {loc.insertIndex}\n\n" +
+                $"Page: {loc.page}\n\n" +
                 $"=== PAGE TEXT ===\n" +
                 $"{pageText}";
 
-            // Highlight phrase (case insensitive)
-            int index = rtbOutput.Text.IndexOf(phrase, StringComparison.OrdinalIgnoreCase);
+
+            lblVolume.Text = "Volume: " + loc.volume.ToString();
+            tbVolume.Value = loc.volume;
+            lblShelf.Text = "Shelf: " + loc.shelf.ToString();
+            tbShelf.Value = loc.shelf;
+            lblWall.Text = "Wall: " + loc.wall.ToString();
+            tbWall.Value = loc.wall;
+            pageRtb.Text = loc.page.ToString();
+            rtbHex.Text = loc.hex;
+
+
+            // Highlight
+            int index = rtbOutput.Text.IndexOf(phrase, StringComparison.Ordinal);
             if (index >= 0)
             {
                 rtbOutput.Select(index, phrase.Length);
@@ -145,7 +183,72 @@ namespace LibraryOfBabel
                 rtbOutput.SelectionBackColor = System.Drawing.Color.Transparent;
                 rtbOutput.Select(0, 0);
             }
+
+            // Save phrase and location for later use
+            lastSearchedPhrase = phrase;
+            lastPhraseLocation = loc;
         }
 
+
+        private void hex_richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void findVolButton2_Click(object sender, EventArgs e)
+        {
+            string hex = rtbHex.Text.Trim();
+            int wall = tbWall.Value;
+            int shelf = tbShelf.Value;
+            int volume = tbVolume.Value;
+            if (!int.TryParse(pageRtb.Text.Trim(), out int page))
+            {
+                MessageBox.Show("Page must be a number.");
+                return;
+            }
+            if (page < 1 || page > 410)
+            {
+                MessageBox.Show("Page number must be between 1 and 410.");
+                return;
+            }
+
+            // Check if this location matches last searched phrase
+            if (lastPhraseLocation.HasValue)
+            {
+                var loc = lastPhraseLocation.Value;
+                if (loc.hex == hex && loc.wall == wall && loc.shelf == shelf && loc.volume == volume && loc.page == page)
+                {
+                    string pageText = GeneratePageWithPhrase(loc.hex, loc.wall, loc.shelf, loc.volume, loc.page, lastSearchedPhrase, loc.insertIndex);
+                    rtbOutput.Text =
+                        $"Location:\nHex: {hex}\nWall: {wall}\nShelf: {shelf}\nVolume: {volume}\nPage: {page}\n\n=== PAGE TEXT ===\n{pageText}";
+                    return;
+                }
+            }
+
+            // Normal page if no phrase at this location
+            GoToPage(hex, wall, shelf, volume, page);
+        }
+
+        private void vol_trackBar3_Scroll(object sender, EventArgs e)
+        {
+
+            lblVolume.Text = $"Volume: {tbVolume.Value}";
+        }
+
+        private void shelf_trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+            lblShelf.Text = $"Shelf: {tbShelf.Value}";
+        }
+
+        private void wall_trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblWall.Text = $"Wall: {tbWall.Value}";
+        }
+
+        private void pageRtb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
