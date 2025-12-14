1e8862ea1cf3ebe1172479a12704d717ff4a6e33
rustjesus <yeric12345@gmail.com>
2025-12-14 09:21:08 -0600

    v0.0.5

--- LibraryOfBabel/Form1.Designer.cs
+++ LibraryOfBabel/Form1.Designer.cs
@@ -102,7 +102,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "v0.0.4";
+            this.label3.Text = "v0.0.5";
             // 
             // tbWall
             // 
